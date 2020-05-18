af6a59eb5b3286bb93c538f978df6d083d8b861f
asosunoff <al.sosunoff@gmail.com>
2020-05-18 20:39:43 +0300

    active quiz

--- src/components/ActiveQuiz/index.js
+++ src/components/ActiveQuiz/index.js
@@ -1,10 +1,22 @@
-import React from 'react';
-import classes from './index.module.css';
+import React from "react";
+import classes from "./index.module.css";
 
-export default () => {
-    return (
-        <div>
-            1
-        </div>
-    )
-}
+export default (props) => (
+    <div className={classes.activeQiuz}>
+        <p className={classes.question}>
+            <span>
+                <strong>2.</strong>&nbsp;
+                Как дела
+            </span>
+
+            <smal>4 из 12</smal>
+        </p>
+
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+            <li>4</li>
+        </ul>
+    </div>
+);
--- src/containers/Quiz/index.js
+++ src/containers/Quiz/index.js
@@ -1,15 +1,19 @@
 import React from "react";
 import classes from "./index.module.css";
+import ActiveQuiz from "../../components/ActiveQuiz";
 
 export default class extends React.Component {
-    state = {
-        quiz: [],
-    }
+	state = {
+		quiz: [],
+	};
 
 	render() {
 		return (
 			<div className={classes.quiz}>
-				<h1>QUIZ</h1>
+				<div className={classes.quizWrapper}>
+					<h1>QUIZ</h1>
+					<ActiveQuiz />
+				</div>
 			</div>
 		);
 	}

08aa6b30f3db54779462c0fe4437219eb7d1e0cb
asosunoff <al.sosunoff@gmail.com>
2020-05-23 10:13:40 +0300

    added page auth, quiz list, quiz creator

---
+++ src/containers/Auth/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import classes from "./index.module.css";
+
+export default class extends React.Component {
+	render() {
+		return (
+			<div>
+				<h1>Auth</h1>
+			</div>
+		);
+	}
+}
---
+++ src/containers/QuizList/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import classes from "./index.module.css";
+
+export default class extends React.Component {
+	render() {
+		return (
+			<div>
+				<h1>Quiz list</h1>
+			</div>
+		);
+	}
+}

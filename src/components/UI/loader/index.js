851570f5d8c8a83578750f6507adad10f8bae4b5
asosunoff <al.sosunoff@gmail.com>
2020-06-04 21:24:58 +0300

    Added component loader

---
+++ src/components/UI/loader/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import classes from "./index.module.css";
+
+export default (props) => {
+	return (
+		<div className={classes.loader}>
+			<div></div>
+			<div></div>
+		</div>
+	);
+};
--- src/containers/QuizList/index.js
+++ src/containers/QuizList/index.js
@@ -1,16 +1,19 @@
 import React from "react";
 import classes from "./index.module.css";
 import { NavLink } from "react-router-dom";
+import Loader from "../../components/UI/loader/";
 
 export default class extends React.Component {
 	state = {
 		quizes: [],
+		loading: true,
 	};
 
 	async componentDidMount() {
 		const { data: list } = await window.axiosTransport.get("quiz/list");
 		this.setState({
 			quizes: list,
+			loading: false,
 		});
 	}
 
@@ -29,7 +32,7 @@ export default class extends React.Component {
 			<div className={classes.quizList}>
 				<h1>Список тестов</h1>
 
-				<ul>{this.renderQuizes()}</ul>
+				{this.state.loading ? <Loader /> : <ul>{this.renderQuizes()}</ul>}
 			</div>
 		);
 	}
